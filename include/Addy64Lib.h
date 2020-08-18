b48a8fee810ac7aacb65c03053c3635a21144a71
Naïm Gómez <33960177+NgomezKS@users.noreply.github.com>
2020-08-19 00:33:19 +0100

    update include/

--- include/Addy64Lib.h
+++ include/Addy64Lib.h
@@ -1,36 +1,39 @@
 #pragma once
+
+#ifndef _Addy64_H_
+#define _Addy64_H_
+
 #include <vector>
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <tchar.h>
 
-#ifndef _Addy64_H
 
-DWORD_PTR baseAddress;
-HANDLE    pHandle;
-DWORD     pID;
-HWND      hGameWindow;
+namespace varAddy64 {
+	DWORD_PTR baseAddress{ 0 };
+	HANDLE    pHandle{ 0 };
+	DWORD     pID{ 0 };
+	HWND      hGameWindow{ 0 };
+	DWORD_PTR  address{ 0 };
+}
 
-void GetModuleBaseAddress(TCHAR* lpszModuleName);
+void getModuleBaseAddress(TCHAR* lpszModuleName);
 
 void getWindowInfo64(char _moduleName[], char _windowName[]);
 
 class Addy64
 {
 
-public:
-	DWORD_PTR address{ 0 };
-
 private:
 	unsigned int size{ 0 };
-	DWORD_PTR aux_address;
-	DWORD_PTR aux_address2;
+	DWORD_PTR aux_address{ 0 };
+	DWORD_PTR aux_address2{ 0 };
 	std::vector<unsigned int> offsets;
 
 public:
 
 	Addy64(std::vector<unsigned int> _offsets)
-	{
+	{			
 		size = _offsets.size();
 		offsets = _offsets;
 		getAddress();
@@ -48,9 +51,9 @@ private:
 	DWORD_PTR getAddress();
 };
 
-void GetModuleBaseAddress(TCHAR* lpszModuleName)
+void getModuleBaseAddress(TCHAR* lpszModuleName)
 {
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID);
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, varAddy64::pID);
 	MODULEENTRY32 ModuleEntry32 = { 0 };
 	ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
 
@@ -59,7 +62,7 @@ void GetModuleBaseAddress(TCHAR* lpszModuleName)
 		do {
 			if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
 			{
-				baseAddress = (DWORD_PTR)ModuleEntry32.modBaseAddr;
+				varAddy64::baseAddress = (DWORD_PTR)ModuleEntry32.modBaseAddr;
 				break;
 			}
 		} while (Module32Next(hSnapshot, &ModuleEntry32));
@@ -70,57 +73,61 @@ void GetModuleBaseAddress(TCHAR* lpszModuleName)
 
 void getWindowInfo64(char _moduleName[], char _windowName[])
 {
-	GetWindowThreadProcessId(FindWindow(NULL, _windowName), &pID);
-	pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
-	GetModuleBaseAddress(_T(_moduleName));
+	GetWindowThreadProcessId(FindWindow(NULL, _windowName), &varAddy64::pID);
+	varAddy64::pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, varAddy64::pID);
+	getModuleBaseAddress(_T(_moduleName));
 }
 
 DWORD_PTR Addy64::getAddress()
-{
+{	
+	int offsetID;
 	if (size == 1)
 	{
-		return baseAddress + offsets[0];
+		return varAddy64::baseAddress + offsets[0];
 	}
 	else if (size > 1)
 	{
-		ReadProcessMemory(pHandle, (LPCVOID)(baseAddress + offsets[0]), &aux_address, sizeof(aux_address), NULL);
+		ReadProcessMemory(varAddy64::pHandle, (LPCVOID)(varAddy64::baseAddress + offsets[0]), &aux_address, sizeof(aux_address), NULL);
 		for (int i = 1; i < size - 1; i++)
 		{
-			ReadProcessMemory(pHandle, (LPCVOID)(aux_address + offsets[i]), &aux_address2, sizeof(aux_address2), NULL);
-			aux_address = aux_address2;		
-			address = aux_address2 + offsets[i + 1];
+			ReadProcessMemory(varAddy64::pHandle, (LPCVOID)(aux_address + offsets[i]), &aux_address2, sizeof(aux_address2), NULL);
+			aux_address = aux_address2;
+			offsetID = offsets[i + 1];
+			varAddy64::address = aux_address2 + offsetID;
 		}
-		return address;
+
+		return varAddy64::address;
 		aux_address = 0;
 	}
+
 }
 
 double    Addy64::getDouble()
 {
-	ReadProcessMemory(pHandle, (LPCVOID)(address), &aux_address, 8, NULL);
+	ReadProcessMemory(varAddy64::pHandle, (LPCVOID)(varAddy64::address), &aux_address, 8, NULL);
 	return *((double*)&aux_address);
 }
 float     Addy64::getFloat()
 {
-	ReadProcessMemory(pHandle, (LPCVOID)(address), &aux_address, 4, NULL);
+	ReadProcessMemory(varAddy64::pHandle, (LPCVOID)(varAddy64::address), &aux_address, 4, NULL);
 	return *((float*)&aux_address);
 }
 int       Addy64::getInt()
 {
-	ReadProcessMemory(pHandle, (LPCVOID)(address), &aux_address, 4, NULL);
+	ReadProcessMemory(varAddy64::pHandle, (LPCVOID)(varAddy64::address), &aux_address, 4, NULL);
 	return *((int*)&aux_address);
 }
 
 void	  Addy64::writeDouble(double value)
 {
-	WriteProcessMemory(pHandle, (LPVOID)(address), &value, sizeof(double), 0);
+	WriteProcessMemory(varAddy64::pHandle, (LPVOID)(varAddy64::address), &value, sizeof(double), 0);
 }
 void	  Addy64::writeFloat(float value)
 {
-	WriteProcessMemory(pHandle, (LPVOID)(address), &value, sizeof(float), 0);
+	WriteProcessMemory(varAddy64::pHandle, (LPVOID)(varAddy64::address), &value, sizeof(float), 0);
 }
 void	  Addy64::writeInt(int value)
 {
-	WriteProcessMemory(pHandle, (LPVOID)(address), &value, sizeof(int), 0);
+	WriteProcessMemory(varAddy64::pHandle, (LPVOID)(varAddy64::address), &value, sizeof(int), 0);
 }
 #endif
